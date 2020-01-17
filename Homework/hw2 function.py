1584c7dd6a80969c7660f2a7e75b16d066ade5c0
Clotonervo <sam.hopkins@blastsoccer.net>
2020-01-16 17:38:22 -0700

    Hw 2

---
+++ Homework/hw2 function.py
@@ -0,0 +1,14 @@
+def modexp(x,y,N):
+    if y==0:
+        return 1
+    z = modexp(x, y//2, N)
+    print("z =",z)
+    print("y =",y);
+    if y % 2 == 0:  #is even
+        return (z**2) % N
+    else:
+        return x * (z**2) % N
+
+
+
+print(modexp(2, 21, 18))
