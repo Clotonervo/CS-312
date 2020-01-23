710e185a25bfb69f6361e6d70393d4008eb19e69
Samuel R. Hopkins <srhopkins@mastercontrol.com>
2020-01-22 18:30:45 -0700

    More work on Project 1

--- Projects/proj1-fermat/fermat.py
+++ Projects/proj1-fermat/fermat.py
@@ -18,16 +18,10 @@ def mod_exp(x, y, N):
 
 
 def fprobability(k):
-    # probability of fermat's theorm being correct is 1/2, and as shown in
-    # class, multiple goes at the formula increases this probability by 1/2^x
-    # where x is the number of values picked.
     return 1- (1/(2**k))
 
 
 def mprobability(k):
-    # probability of miller_rabin's theorm being correct is 3/4, and as shown in
-    # class, multiple goes at the formula increases this probability by 3/4^x
-    # where x is the number of values picked.
     return 1-(1/(4**k))
 
 
@@ -42,20 +36,14 @@ def run_fermat(N,k):
 
 
 def run_miller_rabin(N,k):
-    # You will need to implement this function and change the return value, which should be
-    # either 'prime' or 'composite'.
-    #
-    # To generate random values for a, you will most likley want to use
-    # random.randint(low,hi) which gives a random integer between low and
-    #  hi, inclusive.
 
     for number in range(0, k):
-        a = random.randint(2, N-1)
-        x = (N-1) * 2
+        a = random.randint(1, N-1)
+        x = (N-1)
 
         while x > 1 and x % 2 == 0:
-            x = x/2
             if mod_exp(a, x, N) == 1 or mod_exp(a, x, N) == N-1:
+                x = x/2
                 continue
             else:
                 return 'composite'
