29d3d01bbbf88bd522d1a9fb320067ebca9c8563
Clotonervo <sam.hopkins@blastsoccer.net>
2020-01-23 18:57:39 -0700

    Added comments to code

--- Projects/proj1-fermat/fermat.py
+++ Projects/proj1-fermat/fermat.py
@@ -2,49 +2,48 @@ import random
 
 
 def prime_test(N, k):
-    # This is the main function connected to the Test button. You don't need to touch it.
     return run_fermat(N,k), run_miller_rabin(N,k)
 
 
-def mod_exp(x, y, N):
+def mod_exp(x, y, N):   # Runs in O(n^3), assuming we have n bits for input
     if y==0:
         return 1
     z = mod_exp(x, y//2, N)
-    if y % 2 == 0:      #y is even
-        return (z**2) % N
-    else:               #y is odd
-        return x * (z**2) % N
+    if y % 2 == 0:      # O(1)
+        return (z**2) % N   # Bit shift and then check last bit, O(n^2)
+    else:
+        return x * (z**2) % N   # n^2 + 2n^2 so O(n^2)
 
 
 
-def fprobability(k):
-    return 1-(1/(2**k))
+def fprobability(k):    # Runs in O(n^2) time, while space complexity is O(1)
+    return 1-(1/(2**k)) # Division is O(n^2), while subtraction and division are O(n) and O(2n)
 
 
-def mprobability(k):
-    return 1-(1/(4**k))
+def mprobability(k):    # Runs in O(n^2) time, while space complexity is O(1)
+    return 1-(1/(4**k)) # Division is O(n^2), while subtraction and division are O(n) and O(2n)
 
 
-def run_fermat(N,k):
+def run_fermat(N,k):        # Runs in O(k*n^3) time
     for number in range(k):
-        a = random.randint(2, N-1)
-        if mod_exp(a, N-1, N) == 1: #If mod_exp returns 1, then it is still prime, and we test again
+        a = random.randint(2, N-1) # O(1) time
+        if mod_exp(a, N-1, N) == 1: # mod_exp runs in O(n^3) time
             continue
-        else:                       #If mod_exp != 1, then it is composite
+        else:
             return 'composite'
     return 'prime'
 
 
-def run_miller_rabin(N,k):
-    if N % 2 == 0:
+def run_miller_rabin(N,k):  # Runs in O(n^4*k)
+    if N % 2 == 0:  # O(1) because just needs to check last bit
         return 'composite'
     for number in range(k):
-        a = random.randint(1, N-1)
-        x = (N-1)
-        while x > 1 and x % 2 == 0:
-            if mod_exp(a, x, N) == 1:
-                x = x/2
-            elif mod_exp(a,x,N) == N-1:
+        a = random.randint(1, N-1)  # O(1)
+        x = (N-1)   # O(n) time
+        while x > 1 and x % 2 == 0: # Repeats this at most N times, so O(n)
+            if mod_exp(a, x, N) == 1:   # mod_exp runs in O(n^3) time
+                x = x/2 # O(n^2) for division
+            elif mod_exp(a,x,N) == N-1: # mod_exp runs in O(n^3) time
                 break
             else:
                 return 'composite'
