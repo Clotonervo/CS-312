6c50c6b24998db609b543408b9621b7fb9fb0dd5
Samuel R. Hopkins <srhopkins@mastercontrol.com>
2020-04-03 13:51:17 -0600

    More project 6 questions

--- Projects/proj6-leet-code/Flower-Planting-With-No-Adjacent.py
+++ Projects/proj6-leet-code/Flower-Planting-With-No-Adjacent.py
@@ -1,9 +1,18 @@
 class Solution:
     def gardenNoAdj(self, N: int, paths: List[List[int]]) -> List[int]:
-        colors = [0] * N
-        adj_list = [[]] * N
+        colors = [-1] * N
+        adj_list = [[] for i in range(N)]
         for path in paths:
-            if
-            adj_list[path[0] - 1].append(path[1] - 1)
-            adj_list[path[1] - 1].append(path[0] - 1)
-        print(adj_list)s
+            if (path[1] - 1) not in adj_list[path[0] - 1]:
+                adj_list[path[0] - 1].append(path[1] - 1)
+            if (path[0] - 1) not in adj_list[path[1] - 1]:
+                adj_list[path[1] - 1].append(path[0] - 1)
+        print(adj_list)
+
+        for i in range(N):
+            available_colors = [1,2,3,4]
+            for j in adj_list[i]:
+                if colors[j] in available_colors:
+                    available_colors.remove(colors[j])
+            colors[i] = available_colors.pop()
+        return colors
